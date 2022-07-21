dbc9eba4206bc4a690eaeb865bb13ae8a056d886
instructor <webinstructor19@gmail.com>
2022-07-21 20:31:27 +0200

    'WebFoundation'

---
+++ js/main.js
@@ -0,0 +1,50 @@
+console.log('Hello Javascript')
+
+let user = 'ali'; // undefined
+user = 'sana'
+
+const taxRatio = 5
+
+/**
+ * if(condtion){
+ *  statments
+ * }else if(){
+ * }else{
+ * }
+ */
+
+// let x = 20 , 
+//     y = 50
+
+// if(x > y){
+//     console.log('X is greater than y')
+// }else if(x < y){
+//     console.log('')
+// }else{
+//     console.log('x = y ')
+// }
+
+let age = Number(prompt('Enter Your Age '))
+// if(age >= 18) {
+//     console.log('You Can Signup')
+// }else{
+//     console.log("Sorry You 're Underage")
+// }
+
+let signupMessage = age >= 18 ? 'You Can Signup' : "Sorry You 're Underage";
+console.log(signupMessage)
+
+
+/**\Study
+ * switch ... case 
+ * loops
+ * arrays
+ * objects
+ * functions
+ * 
+ * [ Background Generator App Task ]
+ * 
+ * Http Methods
+ * Understanding Json
+ * What is An Api
+ */
